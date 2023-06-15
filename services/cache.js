e6edfcafcc9b65df07e0fa3c6bf814909db4d16e
naekit <mykyta.onishchuck@code.berlin>
2023-06-15 13:29:39 -0400

    built out caching system to run with exec function

--- services/cache.js
+++ services/cache.js
@@ -1,13 +1,34 @@
 const mongoose = require("mongoose")
+const redis = require("redis")
+const util = require("util")
 
+const redisUrl = "redis://127.0.0.1:6379"
+const client = redis.createClient(redisUrl)
+client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
 mongoose.Query.prototype.exec = async function () {
-	console.log("About to run a query")
+	const key = JSON.stringify(
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name,
+		})
+	)
+	// see if we have val for key in redis
+	const cacheValue = await client.get(key)
 
+	// if we do return that
+	if (cacheValue) {
+		const doc = JSON.parse(cacheValue)
+
+		return Array.isArray(doc)
+			? doc.map((d) => new this.model(d))
+			: new this.model(doc)
+	}
+
+	// else issue query and store result in redis
 	const result = await exec.apply(this, arguments)
 
-	console.log("Just ran a query")
+	client.set(key, JSON.stringify(result))
 
 	return result
 }
