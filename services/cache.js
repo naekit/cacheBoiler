23480770d408e82cf53ea511573f4d2d5ee5b4bb
naekit <mykyta.onishchuck@code.berlin>
2023-06-15 14:09:41 -0400

    added cache clearing for when new post is added

--- services/cache.js
+++ services/cache.js
@@ -4,7 +4,7 @@ const util = require("util")
 
 const redisUrl = "redis://127.0.0.1:6379"
 const client = redis.createClient(redisUrl)
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec
 
 mongoose.Query.prototype.cache = function (options = {}) {
@@ -25,7 +25,7 @@ mongoose.Query.prototype.exec = async function () {
 		})
 	)
 	// see if we have val for key in redis
-	const cacheValue = await client.get(key)
+	const cacheValue = await client.hget(this.hashKey, key)
 
 	// if we do return that
 	if (cacheValue) {
@@ -40,7 +40,13 @@ mongoose.Query.prototype.exec = async function () {
 	// else issue query and store result in redis
 	const result = await exec.apply(this, arguments)
 
-	client.set(key, JSON.stringify(result))
+	client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10)
 
 	return result
+}
+
+module.exports = {
+	clearHash(hashKey) {
+		client.del(JSON.stringify(hashKey))
+	},
 }
