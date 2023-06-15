2a73b123fe9662a1a783587dabd859704322f904
naekit <mykyta.onishchuck@code.berlin>
2023-06-15 13:35:06 -0400

    optional cache chaining function for db queries

--- services/cache.js
+++ services/cache.js
@@ -7,7 +7,18 @@ const client = redis.createClient(redisUrl)
 client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
+mongoose.Query.prototype.cache = function (options = {}) {
+	this.useCache = true
+	this.hashKey = JSON.stringify(options.key || "")
+
+	return this
+}
+
 mongoose.Query.prototype.exec = async function () {
+	if (!this.useCache) {
+		return exec.apply(this, arguments)
+	}
+
 	const key = JSON.stringify(
 		Object.assign({}, this.getQuery(), {
 			collection: this.mongooseCollection.name,
@@ -19,6 +30,7 @@ mongoose.Query.prototype.exec = async function () {
 	// if we do return that
 	if (cacheValue) {
 		const doc = JSON.parse(cacheValue)
+		console.log("FROM CACHE")
 
 		return Array.isArray(doc)
 			? doc.map((d) => new this.model(d))
