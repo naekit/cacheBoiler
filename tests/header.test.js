2ec06937fedf4ead6e4c39bf496ecfde77636614
naekit <mykyta.onishchuck@code.berlin>
2023-06-20 14:45:48 -0400

    testing auth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,31 @@
 const puppeteer = require("puppeteer")
 
-test("adds two numbers", () => {
-	expect(1 + 2).toBe(3)
-})
+let browser, page
 
-test("We can launch a browser", async () => {
-	const browser = await puppeteer.launch({
+beforeEach(async () => {
+	jest.setTimeout(30000)
+	browser = await puppeteer.launch({
 		headless: false,
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
 	})
-	const page = await browser.newPage()
+	page = await browser.newPage()
 	await page.goto("localhost:3000")
+})
+
+afterEach(async () => {
+	await browser.close()
+})
+
+test("Header has correct text", async () => {
 	const text = await page.$eval("a.brand-logo", (el) => el.innerHTML)
 
 	expect(text).toEqual("Blogster")
+})
 
-	await browser.close()
-}, 10000)
+test("Clicking login starts oauth flow", async () => {
+	await page.click(".right a")
+
+	const url = await page.url()
+
+	expect(url).toMatch(/accounts\.google\.com/)
+})
